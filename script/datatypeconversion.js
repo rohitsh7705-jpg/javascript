fbf187d1d3d02ffb9d13b075918b04714916d139
rohitsh7705-jpg <rohit.sh7705@gmail.com>
2025-08-23 14:02:16 +0000

    datatype conversion

---
+++ script/datatypeconversion.js
@@ -0,0 +1,20 @@
+let score= "11"
+console.log(typeof score)
+//let valueinnumber = Number(score)
+//console.log(typeof valueinnumber)
+//console.log(valueinnumber )
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1;
+//false=>0
+let isloggedin = "divya"
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin)
+//1=>true
+// 0=>false
+// "divya"=> true
+let somenumber =11
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof(stringnumber))
