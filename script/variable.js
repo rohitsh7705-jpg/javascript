493e28038de36f870375d46336dfcf62ff7ef76c
rohitsh7705-jpg <rohit.sh7705@gmail.com>
2025-08-23 11:52:38 +0000

    done

---
+++ script/variable.js
@@ -0,0 +1,15 @@
+const accountid = 261106
+let accountemail ="rohit.sh7705@gmail.com"
+var accountpassward = "9506"
+ let accountcity = "basti"
+
+
+
+
+accountemail ="rohit.sh@2630@gmail.com"
+accountpassward = "112606"
+accountcity = "lucknow"
+console.log(accountid);
+console.table([accountid,accountemail,accountpassward,accountcity]);
+
+// prefer not to use of 'var' because if issue in block scope and functional scope
