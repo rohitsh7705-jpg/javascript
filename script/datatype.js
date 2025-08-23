071fdfa0f273fd91cbf2cc0ff12d6ca87626746d
rohitsh7705-jpg <rohit.sh7705@gmail.com>
2025-08-23 13:40:17 +0000

    datatypes

---
+++ script/datatype.js
@@ -0,0 +1,24 @@
+ "use strict" //treat all JS code as newer version
+ //alert(3+3) //  we are using node.js,not broser
+ console.log(3+3) + console.log("Divya")
+ // documentation 1) MDN
+ //               2)tc39
+ let name = "DIvya"
+ let age =18
+ let isloggedin =false
+ 
+ 
+ // number => 2to power 53
+ // bigint
+ // string => ""
+ // boolean => true/ false
+ // null => standalone value
+ // undefined
+ // symbol => unique
+
+
+ // object
+
+
+console.log(typeof null)
+
