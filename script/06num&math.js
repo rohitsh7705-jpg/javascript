aa719318eb68451189a32cead6fa8fb0f6c1c016
rohitsh7705-jpg <rohit.sh7705@gmail.com>
2025-08-24 05:14:25 +0000

    num&math

---
+++ script/06num&math.js
@@ -0,0 +1,34 @@
+const score =100
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+
+ /*Number {100}
+[[Prototype]]
+: 
+Number
+constructor
+: 
+ƒ Number()
+toExponential
+: 
+ƒ toExponential()
+toFixed
+: 
+ƒ toFixed()
+toLocaleString
+: 
+ƒ toLocaleString()
+toPrecision
+: 
+ƒ toPrecision()
+toString
+: 
+ƒ toString()
+valueOf
+: 
+ƒ valueOf() */
+
+console.log(balance.toString().length)
+console.log(balance.toFixed)
